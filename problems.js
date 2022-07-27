670b1ba77ed8b87cc49c1ebaba00a5c7b29a337a
Lloyd Stevens <stevens.lloyd@gmail.com>
2022-07-27 15:52:59 +0100

    fizz buzz

--- problems.js
+++ problems.js
@@ -65,11 +65,23 @@ function fizzBuzz(max) {
     /* Takes a number, max and returns an array that contains every number from
     0 to max (not inclusive) that is divisible by either 3 or 5, **but not both**. */
     // Your code here
+
+    fizzBuzzArray = []
+    for (let i=1; i<max; i++) {
+    
+        if ( (((i%3)==0) && !((i%5)==0)) || (((i%5)==0) && !((i%3)==0)) ) {
+            fizzBuzzArray.push(i);
+        }
+        
+    }
+    return fizzBuzzArray;
+
 };
 
 function hello(name) {
     /* Takes in a string name and returns a string saying "Hello, " to that name. */
     // Your code here
+    
 };
 
 function goodbye(name) {
